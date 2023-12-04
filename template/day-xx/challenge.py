38e56a4bb7e75548e4155cb403fc8aa78fb530b9
Antoine (avs) <avs@odoo.com>
2023-12-04 19:11:27 +0100

    [ADD] Challenge template

---
+++ template/day-xx/challenge.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+
+input_file = Path(__file__).parent / "input.txt"
+
+# --- Part 1 -------------------------------------------------------------------
+
+
+def challenge_01():
+    with input_file.open() as f:
+        pass
+
+
+# --- Part 2 -------------------------------------------------------------------
+
+
+def challenge_02():
+    with input_file.open() as f:
+        pass
